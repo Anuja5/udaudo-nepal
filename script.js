849208bbf5848ccfd69828c2de2209c24350d7c4
Anuja5 <37340610+Anuja5@users.noreply.github.com>
2019-08-12 16:46:35 -0400

    final code

--- script.js
+++ script.js
@@ -1,25 +1,40 @@
+/*---smoooth scrolling--*/
+$(document).ready(function () {
+    $('a[href*="#"]:not([href="#"]):not([data-toggle])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top-80,
+                }, 1000);
+                return false;
+            }
+        }
+    });
+    });
 
 
+/*----news feeds scroll and toggle---*/
+    
     function toggle_visibility(id) {
        var e = document.getElementById(id);
        if(e.style.display == 'block')
        {
           e.style.display = 'none';
+          window.clearInterval(ticker());
 
        }
           
        else
           e.style.display = 'block';
-          pauseanimation();
-         
-          
-
+          window.clearInterval(ticker());
     }
 
     function ticker() {
       $('#feeds').animate({
           top: '-20px'
-      }, 6000, function () {
+      }, 5000, function () {
           $(this).animate({
               top: '0'
           }, 0);
